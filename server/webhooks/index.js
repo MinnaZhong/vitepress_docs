4caf0f858244207185d608e551bd6677c6fa8553
期颐 <1549088785@qq.com>
2024-11-25 11:59:16 +0800

    webhooks

---
+++ server/webhooks/index.js
@@ -0,0 +1,51 @@
+const http = require("http");
+
+const resolvePost = (req) =>
+    new Promise((resolve) => {
+        let chunk = "";
+        req.on("data", (data) => {
+            chunk += data;
+        });
+        req.on("end", () => {
+            resolve(JSON.parse(chunk));
+        });
+    });
+
+function run_cmd(cmd, args, callback) {
+    var spawn = require('child_process').spawn;
+    var child = spawn(cmd, args);
+    var resp = "";
+
+    child.stdout.on('data', function (buffer) {
+        resp += buffer.toString();
+    });
+    child.stdout.on('end', function () {
+        callback(resp)
+    });
+}
+
+http
+    .createServer(async (req, res) => {
+        console.log("receive request");
+        console.log(req.url);
+        if (req.method === "POST" && req.url === "/") {
+            console.log("if true");
+            const data = await resolvePost(req);
+
+            let giturl = data.repository.html_url;
+
+            console.log('next callfile', giturl)
+
+            //callfile.execFile("autobuild.sh", ["giturl", giturl]);
+
+            run_cmd('sh', ['./autobuild.sh'], function (text) {
+                console.log(text)
+            });
+
+            res.end("ok");
+        }
+        console.log("if else");
+    })
+    .listen(3020, () => {
+        console.log("server is ready");
+    });
